76b7158d8b324b08cfeedf6d3e3b3ed9a3872572
Miroslav Bondarev <59547044+Miros77@users.noreply.github.com>
2020-09-20 03:49:00 +0300

    Update ModCakeBlock.java

--- src/main/java/com/miros77/rbp/registry/all_cake/ModCakeBlock.java
+++ src/main/java/com/miros77/rbp/registry/all_cake/ModCakeBlock.java
@@ -1,55 +1,12 @@
 package com.miros77.rbp.registry.all_cake;
 
-import com.google.common.math.Stats;
-
-import net.minecraft.block.BlockState;
 import net.minecraft.block.CakeBlock;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
+//Cake
 
 public class ModCakeBlock extends CakeBlock {
 	public ModCakeBlock(Settings settings) {
 		super(settings);
-	}
-
-@Override
-public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (world.isClient) {
-            ItemStack itemStack = player.getStackInHand(hand);
-            if (this.tryEat(world, pos, state, player).isAccepted()) {
-                return ActionResult.SUCCESS;
-            }
-
-            if (itemStack.isEmpty()) {
-                return ActionResult.CONSUME;
-            }
-        }
- 
-        return this.tryEat(world, pos, state, player);
-	}
+	}	
 	
-	
-@Override
-private ActionResult tryEat(WorldAccess world, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (!player.canConsume(false)) {
-            return ActionResult.PASS;
-        } else {
-            player.incrementStat(Stats.EAT_CAKE_SLICE);
-            player.getHungerManager().add(2, 0.1F);
-            int i = (Integer)state.get(BITES);
-            if (i < 6) {
-                world.setBlockState(pos, (BlockState)state.with(BITES, i + 1), 3);
-            } else {
-                world.removeBlock(pos, false);
-            }
-
-            return ActionResult.SUCCESS;
-        }
-	}
-}	
-
+}
