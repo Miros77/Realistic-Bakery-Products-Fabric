f47cc239397198d3c898793b3e8cb63953252e87
Miros77 <miroslav6002@gmail.com>
2022-04-23 05:14:12 +0300

    add 1.15.2(not realized)

--- src/main/java/rbp/registry/block/cake/RawPizza.java
+++ src/main/java/rbp/registry/block/cake/RawPizza.java
@@ -112,7 +112,7 @@ public class RawPizza extends CakeBlock {
       } else {
          player.incrementStat(Stats.EAT_CAKE_SLICE);
          player.getHungerManager().add(2, 0.2F);
-         player.applyStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 300, 0));
+         player.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 300, 0));
  
          int i = (Integer)state.get(BITES);
          if (i < 6) {
